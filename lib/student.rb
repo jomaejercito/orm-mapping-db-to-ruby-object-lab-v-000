32a74b5caa7ab30fa9720f2872cb35e6695b0c54
Joma Ejercito <jomaejercito@gmail.com>
2018-03-13 21:03:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
       SELECT *
       FROM students
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
   end
 
@@ -114,5 +114,5 @@ class Student
       self.new_from_db(row)
     end
   end
-  
+
 end
